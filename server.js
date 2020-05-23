d6e7458fa33397f1cd2aca8971e2264fd205110a
shanthiECE <171054@nec.edu.in>
2020-05-23 13:25:44 +0530

    moving lab2 code to github

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 var express = require('express');
 var app = express();
 
+var body_parser = require('body-parser');
+
+app.use(body_parser.json());
+
+
+var file = require('fs');
 
 var server = app.listen(8081, function () {
     var host = server.address().address
@@ -21,3 +27,20 @@ var server = app.listen(8081, function () {
        }
        res.end( JSON.stringify(output));
     });
+    
+app.get('/student',function(req,res){
+
+  const data = file.readFileSync('student.json');
+  const dataStr = JSON.parse(data);
+  res.end( JSON.stringify(dataStr));
+
+});
+
+app.post('/add',function(req,res){
+
+  const student  = req.body;
+  console.log('Adding new student: ', student);
+
+  res.end( JSON.stringify("stiudents updated successfully"));
+
+});
